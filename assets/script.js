190c3fb8f0fb8c302897dc367f77311147b3b29e
John Magnuson <sobewon@gmail.com>
2023-02-09 15:16:27 -0500

    added function for quote api, and changed html slightly for testing purposes.

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,34 @@ let quoteBox = document.getElementById("quoteBox");
 //quoteBox.addEventListener("input", function() {
   //console.log(quoteBox.value);
 //});
+var getQuote = function(title) {
+    //var for title?
+    //var title
+    //temp var for title
+    var tempTitle = "attack on titan"
+    var info; //var for data gathered from the api (anime, character, quote)
+    //var for the api link + adding the title of selected anime to the 
+    var searchQ = "https://animechan.vercel.app/api/random/anime?title=" + tempTitle
+    console.log(searchQ) //checking if searchQ working 
 
-
-fetch("https://animechan.vercel.app/api/random/anime?title=naruto")
+    //beginning of fetch for data from api
+    fetch(searchQ)
           .then((response) => response.json())
-          .then((quote) => console.log(quote));
+          .then((quote) => {
+            info = quote
+            console.log(quote)
+            console.log(info)
+
+            //get div where the quote is going
+            var quoteBox = document.getElementById("quoteBox")
+            //set html to the quote
+            quoteBox.innerHTML = ` 
+                <p>Anime: ${info.anime}</p>
+                <p>Character: ${info.character}</p>
+                <p>Quote: ${info.quote}</p>
+                `
+          });
+          
+}
+
+getQuote("naruto")
